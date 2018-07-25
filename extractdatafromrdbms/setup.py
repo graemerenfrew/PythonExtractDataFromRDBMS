8cff4b8a2baa6171ef09cdc5adf1a22721e60b03
Graeme Renfrew <graemerenfrew@Graemes-Air.fritz.box>
2018-07-25 09:18:44 +0200

    initial commit

---
+++ extractdatafromrdbms/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Getting data out of various RDBMS',
+    author='Graeme Renfrew',
+    license='MIT',
+)
